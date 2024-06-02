df3996469e5c9ba3a394e127d6d7c3c17d5f01cc
hs286 <hamzakhalid4992@gmail.com>
2024-06-02 15:15:40 +0500

    done

--- src/Components/BannerSection/Banner.jsx
+++ src/Components/BannerSection/Banner.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import img from "../../images/baneer-img.png"
 
 const Banner = () => {
-    return (
-        <div className='banner-container d-flex align-items-center'>
-            <div className='container '>
+    const containerRef = useRef(null);
+  const [maxWidth, setMaxWidth] = useState('');
 
+  useEffect(() => {
+    const updateMaxWidth = () => {
+      if (containerRef.current) {
+        let computedStyle = window.getComputedStyle(containerRef.current);
+        computedStyle = (+window.innerWidth - parseInt(computedStyle.maxWidth, 10)) / 2
+        setMaxWidth(computedStyle);
+      }
+    };
+
+    updateMaxWidth();
+    window.addEventListener('resize', updateMaxWidth);
+
+    return () => {
+      window.removeEventListener('resize', updateMaxWidth);
+    };
+  }, []);
+  console.log(maxWidth, parseInt(maxWidth, 10), window.innerWidth, )
+    return (
+        <div className='d-flex align-items-center' style={{position: "relative"}}>
+            <img src={img} className='w-100'/>
+            <div   style={{position: "absolute", left:`${maxWidth}px`}}>
+            <div className='container ' ref={containerRef} >
                 <div className="row align-items-center" >
                     <div className="col-12 text-start">
                         <h1 className='quality-text quitos'>Premium Quality <br />
@@ -13,6 +35,7 @@ const Banner = () => {
                             Delivering Top-Quality Service for Luxury Homes in the UAE
                         </small>
                     </div>
+                </div>
                 </div>
             </div>
         </div>
